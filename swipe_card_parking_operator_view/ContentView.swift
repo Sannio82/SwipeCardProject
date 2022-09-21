4b6899d16ef7ead190daf8ac4caf77881c57dfa0
Sannio82 <susanna.johansson@iths.se>
2022-09-21 16:20:19 +0200

    Initial Commit

---
+++ swipe_card_parking_operator_view/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swipe_card_parking_operator_view
+//
+//  Created by Sanna Johansson on 2022-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swipe_card_parking_operator_view/swipe_card_parking_operator_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipe_card_parking_operator_viewApp.swift
+//  swipe_card_parking_operator_view
+//
+//  Created by Sanna Johansson on 2022-09-21.
+//
+
+import SwiftUI
+
+@main
+struct swipe_card_parking_operator_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
